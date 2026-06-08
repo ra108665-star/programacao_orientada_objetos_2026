c4b76b54362938c88a1c89e129b627b556cfe15f
João Choma Neto <jchomax@gmail.com>
2026-06-08 16:59:00 -0300

    semana 14 spring boot restaurante

---
+++ aulas/SEMANA14/restaurante/src/main/java/com/choma/restaurante/model/Pedido.java
@@ -0,0 +1,16 @@
+package com.choma.restaurante.model;
+
+public class Pedido {
+    
+
+    private int id;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+   public int getId() {
+        return id;
+    }
+}
